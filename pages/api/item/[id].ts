2666ef0b5e2bc7ec0443d8f5237f89b239eb24dd
kento <kento@toriwatari.bird>
2022-05-09 21:16:00 +0900

    ✨ #7 item/[id].tsの作成

---
+++ pages/api/item/[id].ts
@@ -0,0 +1,20 @@
+import connectDB from "../../../utils/database"
+import { ItemModel } from "../../../utils/schemaModels"
+
+import type { NextApiHandler } from "next"
+
+const getSingleItem: NextApiHandler = async (req, res) => {
+  try {
+    await connectDB()
+
+    const item = await ItemModel.findById(req.query.id)
+
+    console.log("singleitem", item)
+
+    return res.status(200).send({ message: "アイテム読み取り成功" })
+  } catch (err) {
+    return res.status(400).send({ message: "アイテム読み取り失敗" })
+  }
+}
+
+export default getSingleItem
