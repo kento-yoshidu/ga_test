98426bae03ea2c625a1ba2cccbcc6b1b2e7c9c28
kento <kento@toriwatari.bird>
2022-05-12 20:35:44 +0900

    ✨ item/update/[id].tsの作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,18 @@ const Home: NextPage = () => (
         <button type="submit">登録</button>
       </form>
     </section>
+
+    <section>
+      <h1>Item編集</h1>
+      <form action="http://localhost:3000/api/item/update/6278fa96115ec38f465de5cf" method="POST">
+        タイトル : <input type="text" name="title" /> <br />
+        画像 : <input type="text" name="image" /> <br />
+        価格 : <input type="text" name="price" /> <br />
+        説明 : <input type="text" name="description" /> <br />
+        メールアドレス : <input type="text" name="email" /> <br />
+        <button type="submit">登録</button>
+      </form>
+    </section>
   </>
 )
 
