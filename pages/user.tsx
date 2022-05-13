8a716609bd720b95773a1c2ae40fd01331936914
kento <kento@toriwatari.bird>
2022-05-13 18:59:42 +0900

    ✨ #18 ログイン機能の実装

--- pages/user.tsx
+++ pages/user.tsx
@@ -4,6 +4,14 @@ const UserRegister = () => (
   <>
     <Link href="/">トップへ戻る</Link>
 
+    <h1>ログイン</h1>
+
+    <form action="http://localhost:3000/api/user/login" method="POST">
+      メールアドレス : <input type="text" name="email" /> <br />
+      パスワード : <input type="password" name="password" /> <br />
+      <button type="submit">登録</button>
+    </form>
+
     <h1>ユーザー登録</h1>
 
     <form action="http://localhost:3000/api/user/register" method="POST">
