f9b465e2f8e4231335a318bc1862a9ea0f839678
kento <kento@toriwatari.bird>
2022-05-13 18:41:35 +0900

    ✨ #18 ユーザー登録機能実装

---
+++ pages/userregister.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+
+const UserRegister = () => (
+  <>
+    <Link href="/">トップへ戻る</Link>
+
+    <h1>ユーザー登録</h1>
+
+    <form action="http://localhost:3000/api/user/register" method="POST">
+      名前 : <input type="text" name="name" /> <br />
+      メールアドレス : <input type="text" name="email" /> <br />
+      パスワード : <input type="password" name="password" /> <br />
+      <button type="submit">登録</button>
+    </form>
+  </>
+)
+
+export default UserRegister
