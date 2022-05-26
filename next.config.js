227665dafee3115ecbe82c722f9d241d0face97d
kento <kento@toriwatari.bird>
2022-05-26 20:44:05 +0900

    👨‍⚕️ #32 Bugfix

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
     domains: ["books.google.com"]
   },
   env: {
-    URL: process.env.URL
+    URL: process.env.URL,
+    NEXT_PUBLIC_JWT_SECRET_KEY: process.env.NEXT_PUBLIC_JWT_SECRET_KEY
   }
 }
