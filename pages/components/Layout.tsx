e1206383e7b38a225ef8f5ddbe714c4dcb36234f
kento <kento@toriwatari.bird>
2022-05-28 11:39:06 +0900

    👨‍⚕️ #32 Bugfix

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -1,12 +1,19 @@
 import React from "react"
 
+import Header from "./Header"
+import Footer from "./Footer"
+
 interface Props {
   children: React.ReactNode
 }
 
 const Layout: React.VFC<Props> = ({ children }) => (
   <div>
+    <Header />
+
     { children }
+
+    <Footer />
   </div>
 )
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,53 @@
 import React from "react"
 import Link from "next/link"
+import Head from "next/head"
 
 import ApiFetch from "../lib/apiFetch"
 
-const Home = ({ allItems }: { allItems: Item[] }) => {
-  return (
-    <>
-      {allItems.map((item) => (
-        <Link
-          href={`/item/${item._id}`}
-          key={item.id}
-        >
-          <a>
-            <h2>{item.price}</h2>
-            <h3>{item.title}</h3>
-            <p>{item.description}</p>
-          </a>
-        </Link>
-      ))}
+const Home = ({ allItems }: { allItems: Item[] }) => (
+  <>
+    <Head>
+      <title> 積読改善アプリ</title>
+    </Head>
 
-      <ApiFetch />
+    {allItems.map((item) => (
+      <Link
+        href={`/item/${item._id}`}
+        key={item.id}
+      >
+        <a>
+          <h2>{item.price}</h2>
+          <h3>{item.title}</h3>
+          <p>{item.description}</p>
+        </a>
+      </Link>
+    ))}
 
-      <section>
-        <Link href="/user">ユーザー登録画面へ</Link>
-      </section>
+    <ApiFetch />
 
-      <section>
-        <h1>Item編集</h1>
-        <form action={`${process.env.URL}/api/item/update/627903dd5b3f351b4e0c3cf0`} method="POST">
-          タイトル : <input type="text" name="title" /> <br />
-          画像 : <input type="text" name="image" /> <br />
-          価格 : <input type="text" name="price" /> <br />
-          説明 : <input type="text" name="description" /> <br />
-          <button type="submit">登録</button>
-        </form>
-      </section>
+    <section>
+      <Link href="/user">ユーザー登録画面へ</Link>
+    </section>
 
-      <section>
-        <h1>Item削除</h1>
-        <form action={`${process.env.URL}/api/item/delete/627903dd5b3f351b4e0c3cf0`} method="POST">
-          <button type="submit">削除</button>
-        </form>
-      </section>
-    </>
-  )
-}
+    <section>
+      <h1>Item編集</h1>
+      <form action={`${process.env.URL}/api/item/update/627903dd5b3f351b4e0c3cf0`} method="POST">
+        タイトル : <input type="text" name="title" /> <br />
+        画像 : <input type="text" name="image" /> <br />
+        価格 : <input type="text" name="price" /> <br />
+        説明 : <input type="text" name="description" /> <br />
+        <button type="submit">登録</button>
+      </form>
+    </section>
+
+    <section>
+      <h1>Item削除</h1>
+      <form action={`${process.env.URL}/api/item/delete/627903dd5b3f351b4e0c3cf0`} method="POST">
+        <button type="submit">削除</button>
+      </form>
+    </section>
+  </>
+)
 
 export const getServerSideProps = async () => {
   const response = await fetch(`${process.env.URL}/api/item/readAll`)
