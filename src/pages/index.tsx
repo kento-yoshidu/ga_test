a78b13c801215ec95d0fc08b28ec19b47d6ffc04
kento <kento@toriwatari.bird>
2022-07-30 09:54:19 +0900

    ✨ #75 Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,7 @@
-import Test from "./components/test"
-
 const Home = ({ allItems }: { allItems: Item[] }) => {
   return (
-    <Test />
+    <div>Hello World</div>
   )
-  // useAuth()
-
-  /*
-  return (
-    {/*
-    <div>
-      <Head>
-        <title>積読改善アプリ</title>
-      </Head>
-
-      {allItems.map((item) => (
-        <Link
-          href={`/item/${item._id}`}
-          key={item.id}
-        >
-          <a>
-            <h2>{item.price}</h2>
-            <h3>{item.title}</h3>
-            <p>{item.description}</p>
-          </a>
-        </Link>
-      ))}
-
-      <Test />
-
-      <section>
-        <Link href="/user">ユーザー登録画面へ</Link>
-      </section>
-    </div>
-      */
 }
 
 export const getServerSideProps = async () => {
