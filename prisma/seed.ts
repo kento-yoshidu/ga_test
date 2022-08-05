e8847acecb7fb567d9134a1e70a4c3c0d78e9e36
kento <kento@toriwatari.bird>
2022-08-05 10:08:07 +0900

    ✨ #95 Update

---
+++ prisma/seed.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client"
+import { books } from "../data/books"
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.book.createMany({
+    data: books
+  })
+}
+
+main()
+    .catch((e) => {
+      console.error(e)
+      process.exit(1)
+    })
+    .finally(async () => {
+      await prisma.$disconnect()
+    })
