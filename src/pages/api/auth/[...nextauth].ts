d2b99095c93a9e567844c05f0b05eb155d3ff950
kento <kento@toriwatari.bird>
2022-08-16 19:58:21 +0900

    🌠 Install Packages

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,9 +8,7 @@ export default authHandler
 const options = {
   providers: [
     GithubProvider({
-      /* @ts-ignore */
       clientId: process.env.GITHUB_ID,
-      /* @ts-ignore */
       clientSecret: process.env.GITHUB_SECRET
     })
   ],
