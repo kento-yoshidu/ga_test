5f876f5910b9305a42d56168160c4f4815882298
kento <kento@toriwatari.bird>
2022-08-19 09:21:48 +0900

    ✨ Update

--- prisma/migrations/20220818093849_init/migration.sql
+++ prisma/migrations/20220818093849_init/migration.sql
@@ -3,8 +3,9 @@ CREATE TABLE "Book" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "price" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
     "progress" INTEGER NOT NULL,
+    "flag" BOOLEAN NOT NULL,
 
     CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
 );
