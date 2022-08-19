cc8739ed5a769f355f2b548a844e84e1ca67b03a
kento <kento@toriwatari.bird>
2022-08-19 10:02:07 +0900

    ⚙ #95 テーブル定義変更

--- prisma/migrations/20220819005335_init/migration.sql
+++ prisma/migrations/20220819005335_init/migration.sql
@@ -1,4 +1,13 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Book" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
@@ -9,3 +18,6 @@ CREATE TABLE "Book" (
 
     CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
